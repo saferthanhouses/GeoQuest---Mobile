c091d91b84345bd02b899897de9a6ef1246b0711
saferthanhouses <joey_oliver@hotmail.com>
2015-12-03 16:13:42 -0500

    refactoring mapctrl

---
+++ app/scripts/factories/notificationfactory.js
@@ -0,0 +1,18 @@
+app.factory('UserNotificationFactory', function($cordovaVibration, $cordovaLocalNotification){
+	var factory = {
+		notifyUser : function(message){
+      		$cordovaVibration.vibrate(200);
+      		$cordovaLocalNotification.add({
+        		id: 1,
+        		title: 'GeoQuest Alert!',
+        		text: message,
+        		data: {
+          			customProperty: 'custom value'
+        		}
+      		}).then(function(result){
+        		console.log(result);
+      		});
+    	}
+    }
+    return factory;
+})
