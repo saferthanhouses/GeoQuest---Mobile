7c086751b3086212c0e8801cc6c9d8393436e1f6
saferthanhouses <joey_oliver@hotmail.com>
2015-12-04 11:11:56 -0500

    fixed more conflicts

--- app/scripts/factories/map-factory.js
+++ app/scripts/factories/map-factory.js
@@ -6,11 +6,6 @@ app.factory('MapFactory', function() {
 		generateMap: function(mapElem) {
 			var map = L.map(mapElem, {zoomControl:false});
 
-			// L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-			//     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-			//     subdomains: ['otile1','otile2','otile3','otile4']
-			// }).addTo( map );
-
 			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		        maxZoom: 18,
 		        id: 'scotteggs.o7614jl2',
