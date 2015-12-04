476468820c484494679307713adfc7616ff76512
saferthanhouses <joey_oliver@hotmail.com>
2015-12-04 14:52:54 -0500

    removed comments

--- app/scripts/factories/map-factory.js
+++ app/scripts/factories/map-factory.js
@@ -12,11 +12,6 @@ app.factory('MapFactory', function() {
 		        accessToken: 'pk.eyJ1Ijoic2NvdHRlZ2dzIiwiYSI6ImNpaDZoZzhmdjBjMDZ1cWo5aGcyaXlteTkifQ.LZe0-IBRQmZ0PkQBsYIliw'
 		    }).addTo(map);
 
-			// L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-			//     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-			//     subdomains: ['otile1','otile2','otile3','otile4']
-			// }).addTo( map );
-
 			// disable auto rezoom on watch...
 			map.on('zoomend', changeLocateZoom);
 		    
