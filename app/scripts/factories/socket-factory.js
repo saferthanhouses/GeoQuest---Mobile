e60767edc173270bdf4c5e2a7b39e20c143f25ce
saferthanhouses <joey_oliver@hotmail.com>
2015-12-07 16:31:25 -0500

    shows up when you win

--- app/scripts/factories/socket-factory.js
+++ app/scripts/factories/socket-factory.js
@@ -36,6 +36,7 @@ app.factory('SocketFactory', function($rootScope, $state, ENV) {
 		fellowLocation: function(eventData, fellowArr, myId) {
 			console.log('got a fellow location', eventData);
 			var fellow = eventData.fellow;
+			console.log("eventData", eventData);
             if (fellow.id === myId) return fellowArr;
             for (var i = 0; i < fellowArr.length; i++) {
                 if(fellow.id === fellowArr[i].id) {
@@ -61,6 +62,10 @@ app.factory('SocketFactory', function($rootScope, $state, ENV) {
 			if (nsSocket) nsSocket.disconnect();
 			if (socket) socket.disconnect();
 			$state.go('Home');
+		},
+
+		isWinner: function() {
+			return this.isWinner
 		}
 	};
 
